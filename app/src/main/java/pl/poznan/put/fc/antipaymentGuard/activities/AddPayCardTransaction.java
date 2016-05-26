acb44365097abc04920c86f963569ced6d4d3065
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-05-26 22:51:42 +0200

    Added menu to add transaction activity

--- app/src/main/java/pl/poznan/put/fc/antipaymentGuard/activities/AddPayCardTransaction.java
+++ app/src/main/java/pl/poznan/put/fc/antipaymentGuard/activities/AddPayCardTransaction.java
@@ -3,6 +3,8 @@ package pl.poznan.put.fc.antipaymentGuard.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import pl.poznan.put.fc.antipaymentGuard.R;
 
@@ -16,6 +18,19 @@ public class AddPayCardTransaction extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_add_pay_card_transaction, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
     }
 
 }
