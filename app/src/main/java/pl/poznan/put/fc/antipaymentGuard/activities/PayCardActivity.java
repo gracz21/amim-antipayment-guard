46304485038c58bfb29dd2e98a23363a942d5689
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-05-26 20:58:00 +0200

    Added add transaction activity

--- app/src/main/java/pl/poznan/put/fc/antipaymentGuard/activities/PayCardActivity.java
+++ app/src/main/java/pl/poznan/put/fc/antipaymentGuard/activities/PayCardActivity.java
@@ -1,23 +1,15 @@
 package pl.poznan.put.fc.antipaymentGuard.activities;
 
-import android.support.design.widget.TabLayout;
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-
-import android.widget.TextView;
 
 import pl.poznan.put.fc.antipaymentGuard.R;
 import pl.poznan.put.fc.antipaymentGuard.adapters.PayCardSectionAdapter;
@@ -74,8 +66,7 @@ public class PayCardActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(PayCardActivity.this, AddPayCardTransaction.class));
             }
         });
 
