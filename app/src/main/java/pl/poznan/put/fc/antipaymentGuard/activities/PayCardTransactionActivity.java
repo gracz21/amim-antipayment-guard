1f561afe221db9eaa0fd0ac8232f973e995f679b
Kamil Walkowiak <kamil.walek21@gmail.com>
2016-06-17 23:05:20 +0200

    Added hide for optional info on PayCardTransactionActivity

--- app/src/main/java/pl/poznan/put/fc/antipaymentGuard/activities/PayCardTransactionActivity.java
+++ app/src/main/java/pl/poznan/put/fc/antipaymentGuard/activities/PayCardTransactionActivity.java
@@ -3,6 +3,7 @@ package pl.poznan.put.fc.antipaymentGuard.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -27,8 +28,24 @@ public class PayCardTransactionActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.payCardTextView)).setText(transaction.getPayCard().getName());
         ((TextView) findViewById(R.id.amountTextView)).setText(transaction.getAmountWithCurrencyName());
         ((TextView) findViewById(R.id.dateTextView)).setText(dateFormat.format(transaction.getDate()));
-        ((TextView) findViewById(R.id.placeTextView)).setText(transaction.getPlace());
-        ((TextView) findViewById(R.id.descriptionTextView)).setText(transaction.getDescription());
+
+        TextView placeTextView = (TextView) findViewById(R.id.placeTextView);
+        if(transaction.getPlace() != null && transaction.getPlace().length() > 0) {
+            placeTextView.setText(transaction.getPlace());
+        } else {
+            placeTextView.setVisibility(View.GONE);
+            (findViewById(R.id.placeIconImageView)).setVisibility(View.GONE);
+            (findViewById(R.id.placeLabelTextView)).setVisibility(View.GONE);
+        }
+
+        TextView descriptionTextView = (TextView) findViewById(R.id.descriptionTextView);
+        if(transaction.getDescription() != null & transaction.getDescription().length() > 0) {
+            descriptionTextView.setText(transaction.getDescription());
+        } else {
+            descriptionTextView.setVisibility(View.GONE);
+            (findViewById(R.id.descriptionIconImageView)).setVisibility(View.GONE);
+            (findViewById(R.id.descriptionLabelTextView)).setVisibility(View.GONE);
+        }
     }
 
 }
